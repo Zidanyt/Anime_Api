5f4441d55852e6d49afa48b7da30e20084b1f657
zidany <zidanyteixeira18@gmail.com>
2025-07-24 11:13:32 -0300

    test

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import cors from "cors";
+import serverless from "serverless-http";
 import dotenv from "dotenv";
 
 import animeRoutes from "./routes/anime.routes";
@@ -26,3 +27,5 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`🚀 Server rodando na porta ${PORT}`);
 });
+
+export const handler = serverless(app);
